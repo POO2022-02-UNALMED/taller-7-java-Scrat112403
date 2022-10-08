dc40fe9350932b43c8e0fc0bcb89d2fd39bac750
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-10-07 23:03:27 -0500

    A

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -37,6 +37,6 @@ public class Alfabeto extends Pictograma{
 
     @Override
     public String toString() {
-        return ""A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+        return "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
     }
 }
